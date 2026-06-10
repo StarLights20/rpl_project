4fec1ba9b0f3fa6ef42555ca0dcc72c144b4ff8a
StarLights20 <mbintang908@gmail.com>
2026-06-10 13:43:05 +0700

    fixxxx

--- rpl_project/settings.py
+++ rpl_project/settings.py
@@ -116,3 +116,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
